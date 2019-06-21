54a8479d9de03e8424738c2f7b8e672bc9249470
Navyaa Sharma <48599479+NavyaaSharma@users.noreply.github.com>
2019-06-22 00:58:20 +0530

    models

---
+++ models/db-mongoose.js
@@ -0,0 +1,34 @@
+var mongoose=require('mongoose')
+var validator=require('validator')
+
+mongoose.connect('mongodb://localhost:27017/record',{useNewUrlParser:true})
+
+var users=new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+        lowercase:true,
+        trim:true
+    },
+    email:{
+        type:String,
+        required:true,
+        trim:true,
+        validate(user)
+        {
+            if(!validator.isEmail(user))
+            {
+                alert('invalid email syntax')
+                location.href='/signup'
+            }
+        }
+    },
+    password:{
+        type:String,
+        required:true
+    }
+})
+
+var userRegister=mongoose.model('userRegister',users)
+
+module.exports=userRegister
